25339325485654b40fa6d1abedffd068cacea0ae
Irseora <irseora@gmail.com>
2022-11-10 20:14:51 +0200

    C# project created

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASC_Automat_Vanzari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void AState ()
+        static void BState ()
+        static void CState ()
+        static void DState()
+    }
+}
